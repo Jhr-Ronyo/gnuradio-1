fedb836d8079f8a48d7da1925d25a128aac688fb
jcorgan <jcorgan@221aa14e-8319-0410-a670-987f0aec2ac5>
2009-02-23 22:03:22 +0000

    Merged r10410, r10417 and r10461 from trunk into release 3.2 branch
    git-svn-id: http://nyquist.gnuradio.org/svn/gnuradio/branches/releases/3.2@10484 221aa14e-8319-0410-a670-987f0aec2ac5

--- gnuradio-core/src/lib/missing/posix_memalign.cc
+++ gnuradio-core/src/lib/missing/posix_memalign.cc
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2008 Free Software Foundation, Inc.
+ * Copyright 2008,2009 Free Software Foundation, Inc.
  * 
  * This file is part of GNU Radio
  * 
@@ -25,6 +25,11 @@
 #endif
 
 #include "posix_memalign.h"
+
+#ifdef HAVE_MALLOC_H
+// for Cygwin valloc () prototype
+#include <malloc.h>
+#endif
 
 #ifndef HAVE_POSIX_MEMALIGN
 
