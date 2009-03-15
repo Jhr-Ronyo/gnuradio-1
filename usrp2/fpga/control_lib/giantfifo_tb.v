96347339cefadc4dd3fba1b8664e5f1aa1431d1c
jcorgan <jcorgan@221aa14e-8319-0410-a670-987f0aec2ac5>
2009-03-15 18:01:28 +0000

    Applied changeset r10355:10359 from trunk to release-3.2 branch.
    git-svn-id: http://nyquist.gnuradio.org/svn/gnuradio/branches/releases/3.2@10601 221aa14e-8319-0410-a670-987f0aec2ac5

--- usrp2/fpga/control_lib/giantfifo_tb.v
+++ usrp2/fpga/control_lib/giantfifo_tb.v
@@ -1,38 +1,56 @@
 module fifo_tb();
-   
+
+   localparam WIDTH = 36;
    reg clk, rst;
-   wire short_full, short_empty, long_full, long_empty;
+   wire short_full, short_empty, long_full, long_empty, giant_full, giant_empty;
    wire casc_full, casc_empty, casc2_full, casc2_empty;
    reg 	read, write;
    
-   wire [7:0] short_do, long_do;
-   wire [7:0] casc_do, casc2_do;
-   reg [7:0]  di;
+   wire [WIDTH-1:0] short_do, long_do, casc_do, casc2_do, giant_do;
+   reg [WIDTH-1:0]  di;
 
    reg 	      clear = 0;
    
-   shortfifo #(.WIDTH(8)) shortfifo
+   shortfifo #(.WIDTH(WIDTH)) shortfifo
      (.clk(clk),.rst(rst),.datain(di),.dataout(short_do),.clear(clear),
       .read(read),.write(write),.full(short_full),.empty(short_empty));
    
-   longfifo #(.WIDTH(8), .SIZE(4)) longfifo
+   longfifo #(.WIDTH(WIDTH), .SIZE(4)) longfifo
      (.clk(clk),.rst(rst),.datain(di),.dataout(long_do),.clear(clear),
       .read(read),.write(write),.full(long_full),.empty(long_empty));
    
-   cascadefifo #(.WIDTH(8), .SIZE(4)) cascadefifo
+   cascadefifo #(.WIDTH(WIDTH), .SIZE(4)) cascadefifo
      (.clk(clk),.rst(rst),.datain(di),.dataout(casc_do),.clear(clear),
       .read(read),.write(write),.full(casc_full),.empty(casc_empty));
    
-   cascadefifo2 #(.WIDTH(8), .SIZE(4)) cascadefifo2
+   cascadefifo2 #(.WIDTH(WIDTH), .SIZE(4)) cascadefifo2
      (.clk(clk),.rst(rst),.datain(di),.dataout(casc2_do),.clear(clear),
       .read(read),.write(write),.full(casc2_full),.empty(casc2_empty));
+
+   wire [17:0] RAM_D;
+   wire [18:0] RAM_A;
+   wire        RAM_CLK, RAM_WEn, RAM_LDn, RAM_CE1n, RAM_OEn, RAM_CENn;
+   
+   giantfifo #(.WIDTH(WIDTH)) giantfifo
+     (.clk(clk),.rst(rst),.datain(di),.dataout(giant_do),.clear(clear),
+      .read(read),.write(write),.full(giant_full),.empty(giant_empty),
+      .RAM_D(RAM_D),.RAM_A(RAM_A),.RAM_CE1n(RAM_CE1n),.RAM_CENn(RAM_CENn),
+      .RAM_CLK(RAM_CLK),.RAM_WEn(RAM_WEn),.RAM_OEn(RAM_OEn),.RAM_LDn(RAM_LDn)
+      );
+
+   wire        MODE = 1'b0;
+   cy1356 ram_model(.d(RAM_D),.clk(RAM_CLK),.a(RAM_A),
+		    .bws(2'b00),.we_b(RAM_WEn),.adv_lb(RAM_LDn),
+		    .ce1b(RAM_CE1n),.ce2(1'b1),.ce3b(1'b0),
+		    .oeb(RAM_OEn),.cenb(RAM_CENn),.mode(MODE) 
+		    );
    
    initial rst = 1;
    initial #1000 rst = 0;
    initial clk = 0;
    always #50 clk = ~clk;
    
-   initial di = 8'hAE;
+   initial di = 36'h300AE;
    initial read = 0;
    initial write = 0;
 
@@ -50,7 +68,7 @@ module fifo_tb();
 	  $display("Error: DATA mismatch");
      end
    
-   initial $dumpfile("fifo_tb.vcd");
+   initial $dumpfile("giantfifo_tb.vcd");
    initial $dumpvars(0,fifo_tb);
 
    initial
